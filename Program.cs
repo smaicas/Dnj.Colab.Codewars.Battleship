9c64b8e9776c7f8e19b7474940798a919f998644
smaicas <samuel.maicas.development@gmail.com>
2022-11-27 22:21:44 +0100

    Create sub repository battleship

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using BattleshipFieldValidator;
+
+//  Write a method that takes a field for well - known
+//  board game "Battleship" as an argument and returns true if it has a valid disposition of ships,
+//  false otherwise.Argument is guaranteed to be 10 * 10 two - dimension array.Elements in the array are numbers,
+//  0 if the cell is free and 1 if occupied by ship.
+//  https://www.codewars.com/kata/52bb6539a4cf1b12d90005b7
+
+int[,] buenField = new int[10, 10]
+{
+    {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+    {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+    {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+int[,] malField = new int[10, 10]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+};
+
+Console.WriteLine($"Campo válido {malField.Validate()}");
+
